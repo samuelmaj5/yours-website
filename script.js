b3ad4bc0a8680281f83750735308680c7e5e0488
samuelmaj5 <samuel.majbruch@polytechnique.edu>
2025-03-16 16:03:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -180,16 +180,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const cursor = document.createElement("div");
-    cursor.classList.add("cursor-pixel");
-    document.body.appendChild(cursor);
-
-    document.addEventListener("mousemove", function(e) {
-        cursor.style.left = `${e.clientX}px`;
-        cursor.style.top = `${e.clientY}px`;
-    });
-});
 
 
 
